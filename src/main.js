6e1b4d661194a595fe05105e0cc1c2600dca034b
mosavi87 <sayedmahdi87@gmail.com>
2025-03-26 16:08:04 +0430

    create simple posts header

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import Home from './Pages/Home.vue'
 import About from './Pages/About.vue'
+import Posts from './Pages/Posts.vue'
 import NotFound from './Pages/NotFound.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes =[
     {path: '/', component:Home},
     {path: '/about', component:About},
+    {path: '/posts', component:Posts},
     {path: '/:PathMatch(.*)', component:NotFound},
 ];
 
