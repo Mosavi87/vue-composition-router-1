22a75bcecc408b52ea8995b252856edcf5cd45ea
mosavi87 <sayedmahdi87@gmail.com>
2025-03-26 15:27:28 +0430

    video-52 vue-router-library

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,14 @@
 <template>
   <div>
     <ul>
-      <li><a href="#/">Home</a></li>
-      <li><a href="#/about">About</a></li>
-      <li><a href="#/asd">Broken link</a></li>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/Notkns">NotFound</router-link></li>
     </ul>
 
     <div>
-      <component :is="currentView"></component>
+      <router-view></router-view>
     </div>
   </div>
 
 </template>
-
-
-<script setup>
-import { computed, ref } from 'vue';
-import About from './Pages/About.vue';
-import Home from './Pages/Home.vue';
-import NotFound from './Pages/NotFound.vue';
-
-
-const routes = {
-  '/':Home,
-  '/about':About
-}
-
-const currentPath = ref(window.location.hash)
-
-window.addEventListener('hashChange', () => {
-  currentPath.value =window.location.hash
-});
-
-const currentView = computed(() => {
-  return routes [currentPath.value.slice(1) || '/'] || NotFound;
-});
-
-</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Home from './Pages/Home.vue'
+import About from './Pages/About.vue'
+import NotFound from './Pages/NotFound.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes =[
+    {path: '/', component:Home},
+    {path: '/about', component:About},
+    {path: '/:PathMatch(.*)', component:NotFound},
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
 
 const app = createApp(App)
-
+app.use(router)
 app.mount('#app')
